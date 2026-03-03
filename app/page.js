c185a941c7b7f8c1f7daab4e7aaf6216755bab32
Anil <139544687+anil0123456@users.noreply.github.com>
2026-03-03 16:48:26 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "40px", textAlign: "center" }}>
+      <h1>Thuraka Anil</h1>
+      <h2>Data Analyst</h2>
+
+      <p>
+        Results-driven Data Analyst skilled in SQL, Python, Power BI and Tableau.
+      </p>
+
+      <h3>Skills</h3>
+      <p>Python | SQL | Power BI | Tableau | Excel</p>
+
+      <h3>Experience</h3>
+      <p>
+        Data Analyst Intern – Veloxi Vision Pvt. Ltd. (Dec 2024 – Jun 2025)
+      </p>
+
+      <h3>Contact</h3>
+      <p>Email: thurakaanil0123@gmail.com</p>
+      <p>Phone: +91-7993799084</p>
+    </div>
+  );
+  }
